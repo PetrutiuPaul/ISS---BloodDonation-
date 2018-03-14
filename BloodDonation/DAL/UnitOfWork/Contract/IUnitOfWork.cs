ed3e016083e3ba97301b25b0dab15e6ebdb54641
PetrutiuPaul <paul970410@gmail.com>
2018-03-14 21:05:18 +0200

    add all repositories

--- BloodDonation/DAL/UnitOfWork/Contract/IUnitOfWork.cs
+++ BloodDonation/DAL/UnitOfWork/Contract/IUnitOfWork.cs
@@ -17,6 +17,27 @@ namespace DAL.UnitOfWork.Contract
 
         GenericRepository<Hospital> HospitalRepository { get; }
 
+
+        GenericRepository<Blood> BloodRepository { get; }
+
+        GenericRepository<BloodBank> BloodBankRepository { get; }
+
+        GenericRepository<BloodTestResult> BloodTestResultRepository { get; }
+
+        GenericRepository<County> CountyRepository { get; }
+
+        GenericRepository<Donation> DonationRepository { get; }
+
+        GenericRepository<Locality> LocalityRepository { get; }
+
+        GenericRepository<Notification> NotificationRepository { get; }
+
+        GenericRepository<Product> ProductRepository { get; }
+
+        GenericRepository<Request> RequestRepository { get; }
+
+        GenericRepository<UserNotification> UserNotificationRepository { get; }
+
         void Save();
     }
 }
--- BloodDonation/DAL/UnitOfWork/UnitOfWork.cs
+++ BloodDonation/DAL/UnitOfWork/UnitOfWork.cs
@@ -10,8 +10,18 @@
     public class UnitOfWork : IDisposable, IUnitOfWork
     {
         private AppDbContext context = new AppDbContext();
-        private GenericRepository<User> userRepository;
+        private GenericRepository<Blood> bloodRepository;
+        private GenericRepository<BloodBank> bloodBankRepository;
+        private GenericRepository<BloodTestResult> bloodTestResultRepository;
+        private GenericRepository<County> countyRepository;
+        private GenericRepository<Donation> donationRepository;
         private GenericRepository<Hospital> hospitalRepository;
+        private GenericRepository<Locality> localityRepository;
+        private GenericRepository<Notification> notificationRepository;
+        private GenericRepository<Product> productRepository;
+        private GenericRepository<Request> requestRepository;
+        private GenericRepository<User> userRepository;
+        private GenericRepository<UserNotification> userNotificationRepository;
 
         private UserStore<User> userStore;
         private UserManager<User> userManager;
@@ -56,6 +66,119 @@
                     this.hospitalRepository = new GenericRepository<Hospital>(context);
                 }
                 return hospitalRepository;
+            }
+        }
+
+        public GenericRepository<Blood> BloodRepository
+        {
+            get
+            {
+                if (this.bloodRepository == null)
+                {
+                    this.bloodRepository = new GenericRepository<Blood>(context);
+                }
+                return bloodRepository;
+            }
+        }
+        public GenericRepository<BloodBank> BloodBankRepository
+        {
+            get
+            {
+                if (this.bloodBankRepository == null)
+                {
+                    this.bloodBankRepository = new GenericRepository<BloodBank>(context);
+                }
+                return bloodBankRepository;
+            }
+        }
+        public GenericRepository<BloodTestResult> BloodTestResultRepository
+        {
+            get
+            {
+                if (this.bloodTestResultRepository == null)
+                {
+                    this.bloodTestResultRepository = new GenericRepository<BloodTestResult>(context);
+                }
+                return bloodTestResultRepository;
+            }
+        }
+        public GenericRepository<County> CountyRepository
+        {
+            get
+            {
+                if (this.countyRepository == null)
+                {
+                    this.countyRepository = new GenericRepository<County>(context);
+                }
+                return countyRepository;
+            }
+        }
+        public GenericRepository<Donation> DonationRepository
+        {
+            get
+            {
+                if (this.donationRepository == null)
+                {
+                    this.donationRepository = new GenericRepository<Donation>(context);
+                }
+                return donationRepository;
+            }
+        }
+        public GenericRepository<Locality> LocalityRepository
+        {
+            get
+            {
+                if (this.localityRepository == null)
+                {
+                    this.localityRepository = new GenericRepository<Locality>(context);
+                }
+                return localityRepository;
+            }
+        }
+  
+        public GenericRepository<Notification> NotificationRepository
+        {
+            get
+            {
+                if (this.notificationRepository == null)
+                {
+                    this.notificationRepository = new GenericRepository<Notification>(context);
+                }
+                return notificationRepository;
+            }
+        }
+
+        public GenericRepository<Product> ProductRepository
+        {
+            get
+            {
+                if (this.productRepository == null)
+                {
+                    this.productRepository = new GenericRepository<Product>(context);
+                }
+                return productRepository;
+            }
+        }
+        public GenericRepository<Request> RequestRepository
+        {
+            get
+            {
+                if (this.requestRepository == null)
+                {
+                    this.requestRepository = new GenericRepository<Request>(context);
+                }
+                return requestRepository;
+            }
+        }
+        public GenericRepository<UserNotification> UserNotificationRepository
+        {
+            get
+            {
+                if (this.userNotificationRepository == null)
+                {
+                    this.userNotificationRepository = new GenericRepository<UserNotification>(context);
+                }
+                return userNotificationRepository;
             }
         }
 
